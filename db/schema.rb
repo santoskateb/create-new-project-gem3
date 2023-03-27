70d45eff39bc73adb94a044ac4915b582d65f96d
Kate <santoskateb02@gmail.com>
2023-03-27 13:41:41 +0800

    create model comment and add reference to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_022925) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_053328) do
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_022925) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
